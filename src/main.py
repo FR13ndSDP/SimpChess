4b08b8bfd2344177df07f43adc7bfd3c98736f71
Fr13ndSDP <mylab_octave@163.com>
2022-02-23 20:27:46 +0800

    Tolerance for invalid and illegal moves

--- src/main.py
+++ src/main.py
@@ -1,32 +1,34 @@
-import random
 import sys
 import re
 from Board import Board
-from MoveNode import MoveNode
 from AI import AI
+from termcolor import colored
+
 WHITE = True
 BLACK = False
 
 
-'''
+"""
 def humanCoordToPosition(coord):
     transTable = str.maketrans("abcdefgh", "12345678")
     coord = coord.translate(transTable)
     coord = [int(c) - 1 for c in coord]
     pos = C(coord[0], coord[1])
     return pos
-'''
+"""
+
 
 def positionToHumanCoord(pos):
     transTable = str.maketrans("01234567", "abcdefgh")
     notation = str(pos[0]).translate(transTable) + str(pos[1] + 1)
     return notation
 
+
 def parseInput(board, notation):
     regexNotation = re.compile("[a-h][1-8][a-h][1-8]")
     if regexNotation.match(notation):
         return moveForCoordinateNotation(board, notation)
-    raise ValueError("Invalid move : %s" % notation)
+    raise ValueError(colored("Invalid move : %s" % notation, "red"))
 
 
 def moveForCoordinateNotation(board, notation):
@@ -36,7 +38,7 @@ def moveForCoordinateNotation(board, notation):
             and positionToHumanCoord(move.newPos) == notation[2:4]
         ):
             return move
-    raise ValueError("Illegal move : %s" % notation)
+    raise ValueError(colored("Illegal move : %s" % notation, "red"))
 
 
 if __name__ == "__main__":
@@ -50,15 +52,19 @@ if __name__ == "__main__":
                 print("CHECKMATE!")
                 sys.exit()
             if playSide == board.currentSide:
-                command = input("your move : ")
-                move = parseInput(board, command)
+                command = input(colored("your move : ", "green"))
+                try:
+                    move = parseInput(board, command)
+                except ValueError as error:
+                    print("%s" % error)
+                    continue
                 board.makeMove(move)
             else:
                 ai.makeBestMove()
                 print(board)
     except KeyboardInterrupt:
         sys.exit()
-    '''
+    """
     moves = board.getAllMovesLegal()
     move_first = random.choice(moves)
     board.makeMove(move_first)
@@ -70,4 +76,4 @@ if __name__ == "__main__":
     moves = board.getAllMovesLegal()
     node = MoveNode(move_second, children=moves, parent=move_first)
     print(node)
-    '''
+    """
